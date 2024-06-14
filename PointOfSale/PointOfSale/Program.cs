18dbf7e427c615f5fa00847ada420670c3c8f27d
Adam Palmer <adampalmer@rocketmortgage.com>
2024-06-13 21:18:38 -0400

    I am totally sleeping after this

--- PointOfSale/PointOfSale/Program.cs
+++ PointOfSale/PointOfSale/Program.cs
@@ -1,2 +1,9 @@
 ﻿Console.WriteLine("I need to fartttttttttttttttttttttt");
 Console.WriteLine("HELLLO");
+
+int sleep = 100;
+
+for (int i = 0; i < sleep; i++)
+{
+    Console.WriteLine("ZzZzZzZ");
+}
